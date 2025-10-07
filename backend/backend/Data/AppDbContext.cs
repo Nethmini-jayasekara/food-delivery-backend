dc79e4ef320828dbc3df8b8dc5affed01240d592
Nethmini-jayasekara <132090104+Nethmini-jayasekara@users.noreply.github.com>
2025-10-07 11:23:03 +0530

    added data file

---
+++ backend/backend/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<Order> Orders { get; set; } = null!;
+    public DbSet<OrderItem> OrderItems { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+}
