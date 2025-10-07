98baab0b16bf231b7b93a680126681584feb4fe5
Nethmini-jayasekara <132090104+Nethmini-jayasekara@users.noreply.github.com>
2025-10-07 11:19:24 +0530

    add models

---
+++ backend/backend/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+    }
+}
